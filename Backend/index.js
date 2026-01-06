c36a0950ab6620d21eaf157eae07cf82d4635125
Vedu544 <kulkarnivedant60@gmail.com>
2026-01-06 14:14:46 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,18 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" }); // MUST be first
 
-import {app} from "./app.js";
+import { app } from "./app.js";
 import pgPool from "./config/db.js";
 
 const PORT = process.env.PORT || 8000;
 
-pgPool.query("SELECT NOW()")
+pgPool
+  .query("SELECT NOW()")
   .then(() => {
     console.log("Database connected successfully");
     console.log("JWT_SECRET:", process.env.JWT_SECRET); // DEBUG (temporary)
 
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`🚀 Server running on PORT ${PORT}`);
     });
   })
