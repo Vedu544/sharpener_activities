7727e6fc31add29fa07a526d416c07c3461572b0
Vedu544 <kulkarnivedant60@gmail.com>
2026-01-08 23:05:11 +0530

    started working on backend.

---
+++ Backend/src/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import authRoutes from "../src/routes/auth.routes.js"
+
+const app = express();
+
+/* ---------- MIDDLEWARE ---------- */
+app.use(cors());
+app.use(express.json());
+
+/* ---------- HEALTH CHECK ---------- */
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is healthy 🚀",
+  });
+});
+
+/* ---------- ROUTES (will add later) ---------- */
+app.use("/auth", authRoutes);
+// app.use("/messages", messageRoutes);
+// app.use("/rooms", roomRoutes);
+
+/* ---------- GLOBAL ERROR HANDLER ---------- */
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+
+  res.status(err.statusCode || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
+});
+
+export default app;
