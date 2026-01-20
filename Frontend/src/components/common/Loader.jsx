f610f0fac71f0bc83236c950898f3df449bef1e1
Vedu544 <kulkarnivedant60@gmail.com>
2026-01-20 20:02:02 +0530

    backend is completed just need to test

--- Frontend/src/components/common/Loader.jsx
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-  return (
-    <div className="flex items-center justify-center h-full">
-      <div className="h-8 w-8 animate-spin rounded-full border-4 border-blue-500 border-t-transparent" />
-    </div>
-  );
-};
-
-export default Loader;
--- Frontend/src/utilis/formatTime.js
+++
@@ -1,7 +0,0 @@
-export const formatTime = (date) => {
-  if (!date) return "";
-  return new Date(date).toLocaleTimeString([], {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-};
